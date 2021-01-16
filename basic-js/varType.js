e5be14bb679f857db0a17baec3d44fbca1ed5d9a
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-16 12:18:24 +0600

    added some code in vartype.js file

---
+++ basic-js/varType.js
@@ -0,0 +1,16 @@
+var seenAfter = 21;
+console.log(typeof seenAfter);
+console.log(seenAfter);
+
+var myName = 'joney talukdar';
+console.log(myName);
+
+var phoneName = 'samsung';
+console.log(typeof phoneName);
+
+// boolean
+
+var isHot = true;
+var isRich = false;
+console.log(isHot);
+console.log(typeof isRich);
