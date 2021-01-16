946675c1662a44afe3357c74c18b8d685b612dad
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-16 12:53:35 +0600

    added some code in exploreString.js

---
+++ basic-js/exploreString.js
@@ -0,0 +1,6 @@
+var promise = 'i will work hard to became a programmer';
+
+console.log(promise.toLocaleUpperCase());
+console.log(promise.indexOf('work'));
+console.log(promise.indexOf('became'));
+console.log(promise.split(' '));
