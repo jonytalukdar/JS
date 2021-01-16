51eed23278c50d260931a56d4b75e360f6c8f915
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-16 13:30:28 +0600

    added some code in math.js

---
+++ basic-js/math.js
@@ -0,0 +1,21 @@
+var price1 = 12;
+var price2 = 34;
+var total = price1 + price2;
+console.log(total);
+
+var num1 = 23;
+var num2 = 5;
+console.log(num1 % num2);
+
+var plus = 12;
+plus++;
+console.log(plus);
+
+var minus = 12;
+minus--;
+console.log(minus);
+
+var num1 = 12;
+var num2 = Number('12');
+var result = num1 + num2;
+console.log(result);
