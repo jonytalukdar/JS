0e2fef5382e05327f028e7ca7c95670d5611d255
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-16 12:37:54 +0600

    added some code in reserved.js file

---
+++ basic-js/reserved.js
@@ -0,0 +1,9 @@
+var himName = 'Hero Alam';
+himName = 'Hero Abdul';
+console.log(himName);
+
+var myFirstCodeInJavascript = 'hello world';
+console.log(myFirstCodeInJavascript);
+
+var userName = 'Joney Talukdar';
+console.log(userName);
