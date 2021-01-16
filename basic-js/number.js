b66e648a181b9184f2c28cc1979aba68a3304255
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-16 13:13:21 +0600

    added some code in number.js file

---
+++ basic-js/number.js
@@ -0,0 +1,12 @@
+var num1 = 123;
+num1 = '' + num1;
+var num2 = '311.463';
+num2 = parseInt(num2);
+console.log(num1 + num2);
+
+var num3 = 0.2;
+var num4 = 0.4;
+var total = num3 + num4;
+var fixed = total.toFixed(2);
+console.log(fixed);
+console.log(num3 + num4);
