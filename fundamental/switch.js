b1ada57e20d756b952a55dca60ce730062d409ad
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-17 16:30:38 +0600

    added another statement in switch.js file

--- fundamental/switch.js
+++ fundamental/switch.js
@@ -31,3 +31,21 @@ switch (today) {
     console.log('today is monday');
     break;
 }
+
+const expr = 'Papayas';
+switch (expr) {
+  case 'Oranges':
+    console.log('Oranges are $0.59 a pound.');
+    break;
+
+  case 'Mangoes':
+    console.log('Mongoes is my favourite fruits');
+    break;
+
+  case 'Papayas':
+    console.log('Mangoes and papayas are $2.79 a pound.');
+    break;
+
+  default:
+    console.log(`Sorry, we are out of ${expr}.`);
+}
