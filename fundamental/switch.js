65658635e76ea5df35d98959c6c5ba96399b256b
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-17 16:26:15 +0600

    created a new file called switch.js

---
+++ fundamental/switch.js
@@ -0,0 +1,33 @@
+var date = new Date();
+
+var today = date.getDay();
+
+switch (today) {
+  case 0:
+    console.log('today is sunday');
+    break;
+
+  case 1:
+    console.log('today is monday');
+    break;
+
+  case 2:
+    console.log('today is monday');
+    break;
+
+  case 3:
+    console.log('today is monday');
+    break;
+
+  case 4:
+    console.log('today is monday');
+    break;
+
+  case 5:
+    console.log('today is monday');
+    break;
+
+  case 6:
+    console.log('today is monday');
+    break;
+}
