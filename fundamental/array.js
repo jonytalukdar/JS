578df2c82c7483c3fe3c8fad383529de88ecf166
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-17 12:49:38 +0600

    added some line in array.js file

---
+++ fundamental/array.js
@@ -0,0 +1,9 @@
+var friendsAge = [21, 12, 23, 24, 25, 25];
+var sonaliAge = friendsAge[5];
+var add = friendsAge[0];
+console.log(sonaliAge);
+console.log(friendsAge);
+console.log(friendsAge[0]);
+console.log(friendsAge[5]);
+
+console.log(friendsAge.indexOf(21));
