adc872e3757443baa43b561ed2447d0083fb0e53
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-17 11:40:05 +0600

    added some code in condition.js file

---
+++ basic-js/condition.js
@@ -0,0 +1,20 @@
+var biscuitPrice = 11;
+
+if (biscuitPrice < 10) {
+  console.log('i will eat biscuit');
+} else if (biscuitPrice < 12) {
+  console.log('only tea is real');
+} else {
+  console.log('i Will go to the tea stall');
+}
+
+var jobPaiso = true;
+var savings = 20000;
+
+if (jobPaiso == false && savings > 2000) {
+  console.log('I wil propose you ');
+} else if (jobPaiso == false) {
+  console.log('i will do somehting to get you ');
+} else {
+  console.log('work hard');
+}
