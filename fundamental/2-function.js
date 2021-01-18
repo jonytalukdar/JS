d8dc3ce6ca19037bac8c80de85fed6e31a2a135b
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-18 23:30:11 +0600

    modify function perameter file

---
+++ fundamental/2-function.js
@@ -0,0 +1,17 @@
+function multiple(a) {
+  var result = a * a;
+  return result;
+}
+
+var first = multiple(5);
+var second = multiple(12);
+var total = first + second;
+console.log(total);
+
+function addAll(num1, num2) {
+  var result = num1 + num2;
+  return result;
+}
+
+var sum = addAll(12, 12);
+console.log(sum);
