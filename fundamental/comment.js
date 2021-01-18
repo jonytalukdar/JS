f73201c3e88a621a47181451d1c614fed5c50501
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-18 23:38:00 +0600

    added some line in comment.js

---
+++ fundamental/comment.js
@@ -0,0 +1,5 @@
+// console.log('this is comment line')
+
+/* thisw is multiple comments  tos 
+console.log('this is also in active comments so ) */
+console.log('this is awesome to learnign something new like javascript');
