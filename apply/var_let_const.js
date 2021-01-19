5dca1f53746aabea34df699c63e9d845f8710cdd
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-19 19:31:53 +0600

    added some line in var_let_const

---
+++ apply/var_let_const.js
@@ -0,0 +1,32 @@
+// using var
+
+var myName = 'Joney';
+if (myName.length === 5) {
+  myName = 'Joney Talukdar';
+  console.log(myName);
+} else {
+  myName = 'Jony Talukdar';
+  console.log(myName);
+}
+
+// using let
+
+let herName = 'Jerry';
+if (herName.length === 5) {
+  let herName = 'Jerry Talukdar';
+  console.log(herName);
+} else {
+  let herName = 'Jery Talukdar';
+  console.log(herName);
+}
+
+// using const
+
+const hisName = 'somethig';
+if (hisName.length === 5) {
+  hisName = 'Jerry Talukdar';
+  console.log(hisName);
+} else {
+  hisName = 'Jery Talukdar';
+  console.log(hisName);
+}
