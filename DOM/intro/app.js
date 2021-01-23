cd16fd7b6751ba321eca4d769e119d0b2748a09d
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-23 23:38:10 +0600

    added some line in js file

---
+++ DOM/intro/app.js
@@ -0,0 +1,6 @@
+document.getElementsByTagName('p');
+let first = document.getElementById('first');
+first.style.color = 'black';
+first.style.backgroundColor = 'yellow';
+first.style.fontSize = '45px';
+first.style.padding = ' 10px';
