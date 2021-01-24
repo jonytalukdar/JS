a2c3a068743f20d7f6f600e0d03137b524d76b18
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-24 12:26:14 +0600

    added queryselector

--- DOM/intro/app.js
+++ DOM/intro/app.js
@@ -10,3 +10,14 @@ for (let i = 0; i < authors.length; i++) {
   element.style.padding = '40px';
   element.innerHTML = 'lekhok - ' + (i + 1);
 }
+
+let specials = document.querySelectorAll('.special h3');
+
+for (let i = 0; i < specials.length; i++) {
+  let element2 = specials[i];
+  element2.style.backgroundColor = 'blue';
+  element2.style.color = 'white';
+  element2.style.fontSize = '30px';
+  element2.style.borderRadius = '20px';
+  element2.style.textAlign = 'center';
+}
