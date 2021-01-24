2218e8d3ed0cda12eced9baffc8b98aa77f7e0d3
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-24 12:03:03 +0600

    added getElementByClassName

--- DOM/intro/app.js
+++ DOM/intro/app.js
@@ -1,2 +1,12 @@
-var result = document.getElementsByClassName('author');
-result.style.color = 'red';
+let authors = document.getElementsByClassName('author');
+
+for (let i = 0; i < authors.length; i++) {
+  let element = authors[i];
+  element.style.backgroundColor = 'red';
+  element.style.fontSize = '30px';
+  element.style.borderRadius = '20px';
+  element.style.textAlign = 'center';
+  element.style.margin = '20px';
+  element.style.padding = '40px';
+  element.innerHTML = 'lekhok - ' + (i + 1);
+}
