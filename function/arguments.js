d1dbb24e3f890b48e9e7101c7254fe96e7e9c76c
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-25 12:15:22 +0600

    added some line in arguments.js file

---
+++ function/arguments.js
@@ -0,0 +1,21 @@
+function addNumber(num1, num2) {
+  return num1 + num2;
+}
+let add = addNumber(12, 12);
+console.log(add);
+
+// using arguments
+
+function addAll() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    let element = arguments[i];
+    if (element % 2 == 0) {
+      sum += element;
+    }
+  }
+  return sum;
+}
+
+let result = addAll(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+console.log(result);
