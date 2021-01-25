fc3887919f417753a01670fa36e7d3e8d8db8344
jonytalukdar <jonytalukdar30@gmail.com>
2021-01-25 22:49:01 +0600

    added some line in bubble.js

--- DOM/stopBubble/app.js
+++ DOM/stopBubble/app.js
@@ -1,16 +1,32 @@
-document
-  .getElementById('first-list')
-  .addEventListener('click', function (event) {
-    console.log('I have been clicked');
-    event.stopPropagation();
-  });
+// let items = document.getElementsByClassName('item');
 
-document
-  .getElementById('container')
-  .addEventListener('click', function (event) {
-    console.log('container clicked');
-  });
+// for (let i = 0; i < items.length; i++) {
+//   const item = items[i];
+//   item.addEventListener('click', function (event) {
+//     event.target.parentNode.removeChild(event.target);
+//   });
+// }
+
+document.getElementById('addNew').addEventListener('click', function (event) {
+  let itemParent = document.getElementById('myList');
+  let newItem = document.createElement('li');
+  newItem.innerHTML = 'Brand New item';
+  itemParent.appendChild(newItem);
+});
+
+// document
+//   .getElementById('first-list')
+//   .addEventListener('click', function (event) {
+//     console.log('I have been clicked');
+//     event.stopPropagation();
+//   });
+
+// document
+//   .getElementById('container')
+//   .addEventListener('click', function (event) {
+//     console.log('container clicked');
+//   });
 
 document.getElementById('myList').addEventListener('click', function (event) {
-  console.log('myList clicked');
+  event.target.parentNode.removeChild(event.target);
 });
