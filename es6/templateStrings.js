56d04bf55e5c1ca85a711ca8ebd1d5e68cd332c2
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-01 12:21:05 +0600

    added some code in templateStrings

---
+++ es6/templateStrings.js
@@ -0,0 +1,15 @@
+const firstName = 'Joney';
+const lastName = 'Talukdar';
+
+// const fullName = firstName + ' ' + lastName;
+const fullName = `${firstName} ${lastName} is a good boy. his age is ${21}`;
+
+const loveLetter = 'htoiewhso' + 'hofjhd;osfihs' + 'hsd;klfhsalkfh';
+const lover = `hlaskdjfhsal
+hsflksdhflaksd
+kjlahsdflkasfhlk
+klasjfhsdalk`;
+
+console.log(fullName);
+console.log(loveLetter);
+console.log(lover);
