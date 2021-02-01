fa0d96a590ad2c982b8b38a958df2aec6bc79cd2
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-01 22:53:35 +0600

    added some code in destruecture.js

---
+++ es6/destructure.js
@@ -0,0 +1,33 @@
+// const person = {
+//   name: 'Jack William',
+//   age: 21,
+//   job: 'faceebooker',
+//   gfName: 'ema Waton',
+//   address: 'kochu khet',
+//   phone: 372012320,
+//   friends: ['joney ', 'talukdar', 'jerry ', 'jubayer'],
+// };
+
+// const himGfName = person.gfName;
+// const phone = person.phone;
+
+// const { phone } = person;
+// const { friends } = person;
+// const { name, job } = person;
+// const { age, address, gfName } = person;
+
+// console.log(himGfName);
+// console.log(himGfName);
+// console.log(himGfName);
+// console.log(himGfName);
+// console.log(phone);
+// console.log(friends);
+// console.log(name, job);
+// console.log(age, address, gfName);
+
+// array destrucuture
+
+const friend = ['jeery', 'joney talukdar', 'jubayer', 'jerry', 'bean', 'lucky'];
+
+const [first, second] = friend;
+console.log(first, second);
