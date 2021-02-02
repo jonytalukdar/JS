a4c0259418b97faa0f21c923fc5b644d02ef6dc5
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-02 14:05:54 +0600

    added some line in nullvsUndefined

---
+++ advanced-js/nullVsUndefined.js
@@ -0,0 +1,22 @@
+// let pakhiName;
+// console.log(pakhiName);
+
+// function add(n1, n2) {
+//   console.log(n1 + n2);
+// }
+// const result = add(12, 12);
+// console.log(result);
+
+// function add(n1, n2) {
+//   console.log(n2);
+// }
+// let result = add(12);
+// console.log(result);
+
+let obj = {
+  name: 'joney',
+  age: 21,
+  phone: 3759034,
+};
+
+console.log(obj.address);
