63ccb947c85c5ac00ff2a6b2d2e716a83668565d
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-02 13:53:29 +0600

    added some code in truty and false value

---
+++ advanced-js/truthy-falsy.js
@@ -0,0 +1,22 @@
+// false == null , undefined , Nan , empty strings
+//  true == number , strings , white space , [] , {}
+
+// // const age = 12;
+// const myName = ' ';
+
+// if (myName) {
+//   console.log('condition is true');
+// } else {
+//   console.log('the condition is false');
+// }
+
+// let myName = null;
+// console.log(myName);
+
+// let myName = [];
+let myName = 0;
+if (myName || myName == 0) {
+  console.log('the condition is true');
+} else {
+  console.log('the condition is false');
+}
