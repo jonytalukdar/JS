8af8213fc067ac996030407a2c299a3beb159a7b
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-02 16:35:48 +0600

    added some line in arraMethod.js

---
+++ advanced-js/arrayMethod.js
@@ -0,0 +1,11 @@
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const part = num.slice(2, 6);
+// console.log(part);
+
+// const removed = num.splice(3, 4);
+console.log(num);
+const removed2 = num.splice(3, 4);
+
+// console.log(removed);
+console.log(removed2);
