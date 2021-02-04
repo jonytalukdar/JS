371b4b050d10f67553ac4cd7408ae0ca0d95822d
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-04 16:19:05 +0600

    added some line

---
+++ weathrMap/app.js
@@ -0,0 +1,24 @@
+const apiKey = '08d1ff27ba3f8236c3b68e4278207732';
+const apiBase = 'https://api.openweathermap.org/data/2.5/weather';
+
+function getTemp(city) {
+  const url = `${apiBase}?q=${city}&units=metric&appid=${apiKey}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => updataUi(data));
+}
+const serchButton = document.getElementById('search-button');
+serchButton.addEventListener('click', function () {
+  const cityName = document.getElementById('city').value;
+  getTemp(cityName);
+});
+
+function updataUi(data) {
+  document.getElementById('show-city').innerText =
+    data.name || 'Unknown Location';
+  data.name || 'UnKnown Location';
+  document.getElementById('show-temp').innerText = data.main.temp;
+  document.getElementById('visibility').innerText = data.clouds.all;
+}
+
+getTemp('Dhaka');
