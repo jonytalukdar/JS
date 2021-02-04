33fab024056c502bf198fbb1618978f41c8b7cb2
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-04 22:34:29 +0600

    added some line in objectMethod.js file

---
+++ object/objectmethod.js
@@ -0,0 +1,17 @@
+const normalPerson = {
+  firstName: 'joney',
+  lastName: 'talukdar',
+  salary: 15000,
+  getFullName: function () {
+    console.log(this.firstName + this.lastName);
+  },
+  chargeBill: function (amount) {
+    this.salary -= amount;
+    return this.salary;
+  },
+};
+
+normalPerson.getFullName();
+const result = normalPerson.chargeBill(10000);
+console.log(result);
+normalPerson.chargeBill(120);
