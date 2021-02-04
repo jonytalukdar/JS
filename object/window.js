68097e8797f0b49c3a5f9dfdfdd8e1c9b708e8f3
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-04 23:33:22 +0600

    added some line in window.js

---
+++ object/window.js
@@ -0,0 +1,18 @@
+const myName = 'joney'; // this is global scope
+
+function add(n1, n2) {
+  let result = n1 + n2;
+  console.log(myName);
+  console.log(result);
+  function double(num) {
+    const total = num * n2;
+    return total;
+  }
+  const doubleResult = double(4);
+  console.log(doubleResult);
+
+  return result;
+}
+// console.log(result); this is not global scope
+let sum = add(12, 12);
+console.log(sum);
