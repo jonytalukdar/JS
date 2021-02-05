408baf7592a35da3febaab7046e5364bc3245d44
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-05 16:15:46 +0600

    added setinterval

--- async-await/set.js
+++ async-await/set.js
@@ -2,15 +2,19 @@ function doSomething() {
   console.log('doo');
 }
 
-console.log('first');
-console.log('second');
-console.log('third');
-setTimeout(doSomething, 2000);
-setTimeout(function () {
-  console.log('very lazy');
-}, 3000);
-setTimeout(() => {
-  console.log('its too lazy');
-}, 4000);
-console.log('fourth');
-console.log('sixth');
+// console.log('first');
+// console.log('second');
+// console.log('third');
+// setTimeout(doSomething, 2000);
+// setTimeout(function () {
+//   console.log('very lazy');
+// }, 3000);
+// setTimeout(() => {
+//   console.log('its too lazy');
+// }, 4000);
+// console.log('fourth');
+// console.log('sixth');
+
+setInterval(function () {
+  console.log('hi');
+}, 2000);
