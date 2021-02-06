0b4bc6ed64fe19c738f3095645271411cf351357
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-06 15:22:15 +0600

    added some line in

--- restCountries/app.js
+++ restCountries/app.js
@@ -19,4 +19,7 @@ function updateUI(data) {
   document.getElementById('capital').innerText = data[0].capital;
   document.getElementById('population').innerText = data[0].population;
   document.getElementById('region').innerText = data[0].region;
+  document.getElementById('alt').innerText = data[0].altSpellings[1];
+  document.getElementById('language').innerText = data[0].languages[0].name;
+  document.getElementById('native').innerText = data[0].languages[0].nativeName;
 }
