1ba51df3abea1abef1b1abef84cb8cc10d2836f7
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-06 12:18:06 +0600

    added button event

--- restCountries/app.js
+++ restCountries/app.js
@@ -4,7 +4,14 @@ function getCountyName(city) {
   const url = `https://restcountries.eu/rest/v2/name/${city}`;
   fetch(url)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => console.log(data[0].name));
 }
 
-getCountyName('saudi');
+// added button handler
+
+document.getElementById('search-button').addEventListener('click', function () {
+  const cityName = document.getElementById('input').value;
+  getCountyName(cityName);
+});
+
+function updateUI(data) {}
