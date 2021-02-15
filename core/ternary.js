8446003afc7984232c02b85781fd350248d1be0b
jonytalukdar <jonytalukdar30@gmail.com>
2021-02-15 23:16:30 +0600

    added some line in ternary.js file

---
+++ core/ternary.js
@@ -0,0 +1,24 @@
+//  ternary operator
+const name = 'joney';
+const age = 21;
+const result = age > 18 ? 'you are adult' : 'you are not adult';
+console.log(result);
+
+const active = true;
+const userActive = active ? 'user are active' : 'user are not active';
+console.log(userActive);
+
+//  ternary function call shortcut
+// active ? display() : displayNone();
+
+// active && display();
+const isActive = active && 5;
+console.log(isActive);
+
+//  string to number
+const n = +'23';
+console.log(n);
+
+// number to string
+const n1 = 12 + '';
+console.log(n1);
